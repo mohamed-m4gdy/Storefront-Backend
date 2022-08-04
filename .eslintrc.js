78a48c16aef7df251ffda4ef2fb7d5d83475f2c6
xuncharted <evilonlyone@gmail.com>
2022-08-04 02:11:53 -0700

    init project with typescript and added prettier and eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 2, // means error
+    semi: ['error', 'never'],
+    quotes: ['error', 'single'],
+    'no-console': 0, // means warning
+    'no-var': 'error',
+    'prefer-const': 'error',
+  },
+}
